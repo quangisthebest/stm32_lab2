2079ffb0efeb926c632baae6f624e3e69d0ecf6b
quangisthebest <99035741+quangisthebest@users.noreply.github.com>
2025-10-08 14:50:29 +0700

    ex1

--- source code/Core/Src/main.c
+++ source code/Core/Src/main.c
@@ -50,7 +50,6 @@ TIM_HandleTypeDef htim2;
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_TIM2_Init(void);
-HAL_B
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -90,7 +89,7 @@ int main(void)
   MX_GPIO_Init();
   MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
-
+  HAL_TIM_Base_Start_IT(&htim2);
   /* USER CODE END 2 */
 
   /* Infinite loop */
